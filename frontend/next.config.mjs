11a82438eecb1f89881899df20db398489fa03fd
kungslowjam <kungslowjam@gmail.com>
2025-08-04 20:48:18 +0930

    sdsd

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -18,7 +18,6 @@ const nextConfig = {
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     minimumCacheTTL: 60,
   },
-  output: 'standalone',
   experimental: {
     serverComponentsExternalPackages: ['@supabase/supabase-js'],
   },
